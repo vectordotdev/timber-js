dbf5e09eb5a260afb9629f16d0049d5c925119e6
Lee Benson <lee@leebenson.com>
2018-11-20 15:51:57 +0000

    exports `Queue`

--- packages/npm-tools/src/index.ts
+++ packages/npm-tools/src/index.ts
@@ -1,9 +1,12 @@
 import { IQueue } from "./types";
+import Queue from "./queue";
 import makeThrottle from "./throttle";
 
 export {
   // Types
   IQueue,
+  // Classes
+  Queue,
   // Functions
   makeThrottle
 };
