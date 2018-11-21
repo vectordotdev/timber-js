68a32e8bd50caaa2a133b59d105c6f3842ba49d4
Lee Benson <lee@leebenson.com>
2018-11-21 20:58:43 +0000

    0.7.0 - Node logging

--- src/entry/node.ts
+++ src/entry/node.ts
@@ -1,12 +1,39 @@
+import fetch from "node-fetch";
+
+import { makeThrottle, base64Encode } from "@timberio/tools";
+import { ITimberLog, Pipeline } from "../lib/types";
+
 import Base from "../lib/base";
 
-class NodeLogger extends Base {
+class BrowserLogger extends Base {
   public constructor(apiKey: string) {
     super(apiKey);
-    console.log("Hello from Node.js!");
 
-    this.setSync(async log => log);
+    // TODO - remove this in production... dump out the env for dev!
+    console.log("Hello from Node!");
+
+    // Create a sync throttler
+    const throttler = makeThrottle<Pipeline>(5);
+
+    // Sync function
+    const sync = async (log: ITimberLog): Promise<ITimberLog> => {
+      // TODO - obviously, this doesn't conform perfectly to the spec
+      // yet... dev only!
+      await fetch("https://logs.timber.io/frames", {
+        method: "POST",
+        headers: {
+          "Content-Type": "text/plain",
+          Authorization: `Basic ${base64Encode(this._apiKey)}`
+        },
+        body: `debug: ${log.message}`
+      });
+
+      return log;
+    };
+
+    // Set the throttled sync function
+    this.setSync(throttler(sync));
   }
 }
 
-export default NodeLogger;
+export default BrowserLogger;
