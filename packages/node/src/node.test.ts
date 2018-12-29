7b7e3c5d85355f1e4d25e80c100a757e88c9635b
Lee Benson <lee@leebenson.com>
2018-12-29 08:51:16 +0000

    write streams
    # `@timberio/node`
    
    - adds `.pipe()` function to enable writing synced logs to stream (closes #34, closes #29)
    
    - adds write stream tests

--- packages/node/src/node.test.ts
+++ packages/node/src/node.test.ts
@@ -1,3 +1,8 @@
+import * as os from "os";
+import * as path from "path";
+import * as fs from "fs";
+import { Writable } from "stream";
+
 import nock from "nock";
 import { ITimberLog, LogLevel } from "@timberio/types";
 
@@ -23,7 +28,7 @@ describe("node tests", () => {
     expect(actualValue).toEqual(expectedValue);
   });
 
-  it("should echo log if timber sends 20x status code", async done => {
+  it("should echo log if timber sends 20x status code", async () => {
     nock("https://logs.timber.io")
       .post("/frames")
       .reply(201);
@@ -33,11 +38,9 @@ describe("node tests", () => {
     const node = new Node("valid api key");
     const echoedLog = await node.log(message);
     expect(echoedLog.message).toEqual(expectedLog.message);
-
-    done();
   });
 
-  it("should throw error if timber sends non 200 status code", async done => {
+  it("should throw error if timber sends non 200 status code", async () => {
     nock("https://logs.timber.io")
       .post("/frames")
       .reply(401);
@@ -45,7 +48,76 @@ describe("node tests", () => {
     const node = new Node("invalid api key");
     const message: string = String(Math.random);
     await expect(node.log(message)).rejects.toThrow();
+  });
 
-    done();
+  it("should enable piping logs to a writable stream", async () => {
+    // Create a writable stream
+    const writeStream = new Writable({
+      write(
+        chunk: any,
+        encoding: string,
+        callback: (error?: Error | null) => void
+      ): void {
+        // Will be a buffered JSON string -- parse
+        const log: ITimberLog = JSON.parse(chunk.toString());
+
+        // Expect the log to match the message
+        expect(log.message).toEqual(message);
+
+        callback();
+      }
+    });
+
+    // Fixtures
+    const timber = new Node("test");
+    timber.pipe(writeStream);
+
+    const message = "This should be streamed";
+
+    // Mock the sync method by simply returning the same logs
+    timber.setSync(async logs => logs);
+
+    // Fire a log event
+    await timber.log(message);
+  });
+
+  it("should pipe logs to a writable file stream", async done => {
+    // Create a temporary file name
+    const temp = path.join(os.tmpdir(), `timber_${Math.random()}`);
+
+    // Create a write stream based on that temp file
+    const writeStream = fs.createWriteStream(temp);
+
+    // Pass write stream to Timber
+    const timber = new Node("test");
+    timber.pipe(writeStream);
+
+    // Mock the sync method by simply returning the same logs
+    timber.setSync(async logs => logs);
+
+    // Create messages
+    const messages = ["message 1", "message 2"];
+
+    // Log messages
+    await Promise.all(messages.map(msg => timber.log(msg)));
+
+    writeStream.on("finish", () => {
+      // Get the stored data, and translate back to JSON
+      const data = fs
+        .readFileSync(temp)
+        .toString()
+        .trim()
+        .split("\n")
+        .map(line => JSON.parse(line));
+
+      // Messages should match
+      for (let i = 0; i < messages.length; i++) {
+        expect(data[i].message).toEqual(messages[i]);
+      }
+
+      done();
+    });
+
+    writeStream.end();
   });
 });
--- packages/node/src/node.ts
+++ packages/node/src/node.ts
@@ -1,8 +1,10 @@
+import { Duplex, Writable } from "stream";
+
 import fetch from "cross-fetch";
 // import Msgpack from "msgpack5";
 
 import { base64Encode } from "@timberio/tools";
-import { ITimberLog, ITimberOptions } from "@timberio/types";
+import { ITimberLog, ITimberOptions, LogLevel } from "@timberio/types";
 import { Base } from "@timberio/core";
 
 import { getUserAgent } from "./helpers";
@@ -11,6 +13,12 @@ import { getUserAgent } from "./helpers";
 // const msgpack = Msgpack();
 
 export class Node extends Base {
+  /**
+   * Readable/Duplex stream where JSON stringified logs of type `ITimberLog`
+   * will be pushed after syncing
+   */
+  private _writeStream?: Writable | Duplex;
+
   public constructor(apiKey: string, options?: Partial<ITimberOptions>) {
     super(apiKey, options);
 
@@ -44,5 +52,40 @@ export class Node extends Base {
 
     // Set the throttled sync function
     this.setSync(sync);
+  }
+
+  /**
+   * Override `Base` log to enable Node.js streaming
+   *
+   * @param message: string - Log message
+   * @param level (LogLevel) - Level to log at (debug|info|warn|error)
+   * @param log: (Partial<ITimberLog>) - Initial log (optional)
+   * @returns Promise<ITimberLog> after syncing
+   */
+  public async log(
+    message: string,
+    level?: LogLevel,
+    log?: Partial<ITimberLog>
+  ): Promise<ITimberLog> {
+    // Process/sync the log, per `Base` logic
+    const processedLog = await super.log(message, level, log);
+
+    // Push the processed log to the stream, for piping
+    if (this._writeStream) {
+      this._writeStream.write(JSON.stringify(processedLog) + "\n");
+    }
+
+    // Return the transformed log
+    return processedLog;
+  }
+
+  /**
+   * Pipe JSON stringified `ITimberLog` to a stream after syncing
+   *
+   * @param stream - Writable|Duplex stream
+   */
+  public pipe(stream: Writable | Duplex) {
+    this._writeStream = stream;
+    return stream;
   }
 }
